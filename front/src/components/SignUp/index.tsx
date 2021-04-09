819322e505a2aa0862639945ae6e6d1eb8fb076f
matheusmazon <80927667+matheusmazon@users.noreply.github.com>
2021-04-08 21:35:43 -0300

    login e signup

--- front/src/components/SignUp/index.tsx
+++ front/src/components/SignUp/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 //import api from '../../services/api';
 
-import './styles-login.css'
+//import './styles-login.css'
 
-const Login: React.FC = () => {
+const SignUp: React.FC = () => {
 
+    const [name, setName] = useState<HTMLInputElement["value"]>();
     const [email, setEmail] = useState<HTMLInputElement["value"]>();
     const [password, setPassword] = useState<HTMLInputElement["value"]>();
+
 
 
     function handleSubmit(event: { preventDefault: () => void; }) {
@@ -19,13 +20,21 @@ const Login: React.FC = () => {
 
 
     return (
-        <div id="login">
+        <div id="signup">
             <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    required
+                    placeholder="Nome completo"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
                 <input
                     type="text"
                     name="email"
                     required
-                    placeholder="Seu email"
+                    placeholder="Email"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
                 />
@@ -37,12 +46,11 @@ const Login: React.FC = () => {
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                 />
-                <button type="submit">Entrar</button>
+                <button type="submit">Cadastrar</button>
             </form>
-            <Link to="/SignUp">Cadastre-se</Link>
         </div>
     );
 }
 
 
-export default Login;
+export default SignUp;
--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/signup" exact component={SignUp} />
+
             </Switch>
         </BrowserRouter>
     );
